97c3e2f3dd7509354590174696a6c05ef497bb1b
VICTOR KEMBOI <vickemtoo97@gmail.com>
2024-07-30 13:07:25 +0300

    roots

--- server/seed.py
+++ server/seed.py
@@ -13,13 +13,4 @@ def create_roles():
         db.session.add(role)
         db.session.commit()
         
-def get_role_by_name(role_name):
-    # query the role table so as to find a role by its name
-    return Role.querry.filter_by(name=role_name).first()
-
-def create_users(num_users):
-    users=[]
-    student_role=get_role_by_name('student')
-    for _ in range(num_users):
-        users=
         
