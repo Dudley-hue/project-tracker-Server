c13f26ecfb03262b350dcdede49b8c4c13f0d6e7
Brian Maitho <bmaitho@gmail.com>
2024-08-19 10:55:20 +0300

    minor app.py change

--- app.py
+++ app.py
@@ -22,7 +22,8 @@ def create_app():
         db.create_all()  # Create database tables if they don't exist
 
     return app
+app = create_app()
 
 if __name__ == '__main__':
-    app = create_app()
+   
     app.run(debug=True)
